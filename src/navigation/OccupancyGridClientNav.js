5a10793ff3b71d8f348404002ace48f8c585fd7b
Randel Capati <randelmc21@gmail.com>
2022-11-07 14:42:27 +0800

    > forgot to commit

--- src/navigation/OccupancyGridClientNav.js
+++ src/navigation/OccupancyGridClientNav.js
@@ -34,6 +34,7 @@ ROS3D.OccupancyGridClientNav = function(options) {
   this.viewer = options.viewer || null;
   this.navServerName = options.navServerName || '/move_base';
   this.navActionName = options.navActionName || 'move_base_msgs/MoveBaseAction';
+  this.navigatorInitState = options.navigatorInitState || true;
 };
 ROS3D.OccupancyGridClientNav.prototype.__proto__ = ROS3D.OccupancyGridClient.prototype;
 
@@ -60,6 +61,7 @@ ROS3D.OccupancyGridClientNav.prototype.processMessage = function(message){
     serverName: this.navServerName,
     actionName: this.navActionName,
     occupancyGridFrameID: message.header.frame_id,      // this should be the same frame id as OccupancyGridNav
+    navigatorInitState: this.navigatorInitState,
 
   });
 
